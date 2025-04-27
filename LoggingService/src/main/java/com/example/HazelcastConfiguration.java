f3d90ee3d6901cff4d0129b40a8ea12a74703f6f
Kostyantin1408 <kostyantin1408@gmail.com>
2025-04-27 16:45:45 +0300

    update

--- LoggingService/src/main/java/com/example/HazelcastConfiguration.java
+++ LoggingService/src/main/java/com/example/HazelcastConfiguration.java
@@ -5,9 +5,13 @@ import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.config.JoinConfig;
 import com.hazelcast.config.QueueConfig;
 import com.hazelcast.core.Hazelcast;
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.KeyValueClient;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
 
 @Configuration
 public class HazelcastConfiguration {
@@ -15,6 +19,14 @@ public class HazelcastConfiguration {
     @Bean
     public HazelcastInstance hazelcastInstance() {
         Config config = new Config();
+        Consul consul = Consul.builder().build();
+        KeyValueClient kvClient = consul.keyValueClient();
+        Optional<String> logging_server_1 = kvClient.getValueAsString("config/logging-service-1");
+        Optional<String> logging_server_2 = kvClient.getValueAsString("config/logging-service-2");
+        Optional<String> logging_server_3 = kvClient.getValueAsString("config/logging-service-3");
+        Optional<String> facade_server = kvClient.getValueAsString("config/facade-service");
+        Optional<String> message_server_1 = kvClient.getValueAsString("config/message-service-1");
+        Optional<String> message_server_2 = kvClient.getValueAsString("config/message-service-2");
         config.setInstanceName("logging-service");
 
         config.setClusterName("logging-cluster");
@@ -31,12 +43,12 @@ public class HazelcastConfiguration {
         joinConfig.getMulticastConfig().setEnabled(false);
         joinConfig.getTcpIpConfig()
                 .setEnabled(true)
-                .addMember("169.254.198.8:5742")
-                .addMember("169.254.198.8:5743")
-                .addMember("169.254.198.8:5744")
-                .addMember("169.254.198.8:5745")
-                .addMember("169.254.198.8:5746")
-                .addMember("169.254.198.8:5747");
+                .addMember(logging_server_1.get())
+                .addMember(logging_server_2.get())
+                .addMember(logging_server_3.get())
+                .addMember(facade_server.get())
+                .addMember(message_server_1.get())
+                .addMember(message_server_2.get());
 
         config.getJetConfig().setEnabled(true);
 
--- MessageService/src/main/java/com/example/MessageController.java
+++ MessageService/src/main/java/com/example/MessageController.java
@@ -2,12 +2,16 @@ package com.example;
 
 import com.hazelcast.collection.IQueue;
 import com.hazelcast.core.HazelcastInstance;
+import com.orbitz.consul.KeyValueClient;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.orbitz.consul.Consul;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MessageController{
@@ -23,7 +27,11 @@ public class MessageController{
     @PostConstruct
     public void startConsumer() {
         new Thread(() -> {
-            IQueue<String> queue = hazelcastInstance.getQueue("queue");
+            Consul consul = Consul.builder().build();
+            KeyValueClient kvClient = consul.keyValueClient();
+            Optional<String> queue_name = kvClient.getValueAsString("queue-name");
+            IQueue<String> queue;
+            queue = queue_name.<IQueue<String>>map(hazelcastInstance::getQueue).orElseGet(() -> hazelcastInstance.getQueue("queue"));
             while (true) {
                 try {
                     String message = queue.take();
